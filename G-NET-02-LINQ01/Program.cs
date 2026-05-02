5e4837dd296659131e2bad07ebfa8c7d3247c00e
Mary-Raafat <morotadrous@gmail.com>
2026-05-02 15:18:36 +0300

    Q1

--- G-NET-02-LINQ01/Program.cs
+++ G-NET-02-LINQ01/Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿using G_NET_02_LINQ01.DataSource;
+
+#region Q1
+//var result = Source.ProductList.Where(p => p.Category == "Seafood").Select(p => new
+//{
+//    p.ProductName,
+//    p.UnitPrice
+//});
+//foreach (var item in result)
+//{
+//    Console.WriteLine(item);
+//}
+#endregion
+
+
